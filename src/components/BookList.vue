e08a21a16bdfea25567fe0330b8bb55e949d7d40
Jose Luis Cambero <jlcambero@gmail.com>
2019-03-11 12:15:44 +0100

    Creating the BookListComponent

---
+++ src/components/BookList.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'BookList',
+  data() {
+    return {
+      title: 'All Books'
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+</style>
